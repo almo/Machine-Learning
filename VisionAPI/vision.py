0371eb488374f1a13d4b3bb46170adf28637809d
Andres Leonardo Martinez Ortiz a.k.a almo <aleonar@gmail.com>
2016-11-04 23:58:08 +0100

    Processing a list of Google+ IDs as arguments, getting labels for profile picture

--- VisionAPI/vision.py
+++ VisionAPI/vision.py
@@ -5,30 +5,56 @@
 
 import argparse
 import json
+import urllib
+import base64
 
 from googleapiclient import discovery
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('image_uri', help='The image URI you\'d like to label.')
-    args = parser.parse_args()
-
-    api_key = json.load(open('/usr/local/google/home/almo/Dev/keys/ex1/api_key.json'))['api_key']
+def analyze_img(image_uri):
+    api_key = json.load(open('/home/almo/Dev/keys/ex1/api_key.json'))['api_key']
 
     service = discovery.build('vision','v1',developerKey=api_key)
     
+    image= urllib.urlopen(image_uri)
+    image_content = base64.b64encode(image.read())
+    
     service_request = service.images().annotate(body={
-        'requests':[
-            {
-                'image':{'source':{'gcsImageUri':args.image_uri}
+        'requests': [{
+            'image': {
+                'content': image_content.decode('UTF-8')
                 },
-                'features':[
-                    {'type':'FACE_DETECTION','maxResults':10},
-                    {'type':'LABEL_DETECTION','maxResults':10},
-                    {'type':'SAFE_SEARCH_DETECTION','maxResults':10}
-                    ]}]
+            'features': [{
+                'type': 'LABEL_DETECTION',
+                'maxResults': 3
+                }]
+            }]
         })
     
     response = service_request.execute()
+    return response
 
-    print('%s' % response)
+def get_plus_profile(plus_id):
+    api_key = json.load(open('/home/almo/Dev/keys/ex1/api_key.json'))['api_key']
+    
+    service = discovery.build('plus','v1',developerKey=api_key)
+    service_request = service.people().get(userId = plus_id)
+    response = service_request.execute()
+    return response
+    
+    return 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('plus_ids', nargs='+', help='Google+ profiles you\'d like to analyze')
+    args = parser.parse_args()
+
+    for plus_id in args.plus_ids:  
+        plus_profile = get_plus_profile(plus_id)
+        
+        image_uri = plus_profile['image']['url'].replace("?sz=50","?sz=250")
+    
+        image_data = analyze_img(image_uri)
+
+        print(image_uri)
+        for label in image_data['responses'][0]['labelAnnotations']:
+            print(label['description'],label['score']) 
