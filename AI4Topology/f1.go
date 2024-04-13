3464be0c34be292321a62156ac42398de9231e8f
Andres-Leonardo Martinez-Ortiz <almo@andreslardosmbp.lan>
2024-04-13 12:25:17 +0200

    Definition of tVertex and tPolygon

--- AI4Topology/f1.go
+++ AI4Topology/f1.go
@@ -1,19 +1,16 @@
-/*
-Copyright 2024 Andres Leonardo Martinez Ortiz
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-package main
-*/
+// Copyright 2024 Andres Leonardo Martinez Ortiz
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package main
 
 import (
@@ -21,22 +18,40 @@ import (
 	"math/rand"
 )
 
-/**
- * Const definition X & Y (dimensions)
- */
+// Const definition X & Y (dimensions)
 const (
-	X   = iota /* 0 */
-	Y          /* 1 */
-	DIM        /* 2 */
+	X   = iota // 0
+	Y          // 1
+	DIM        // 2
 )
 
-/**
- * Type tpoint (topological point) define as a bidimensional (X, Y)
- * array of floats
- */
-type tpoint [DIM]float64
+// Type tpoint (topological point) define as a bidimensional (X, Y)
+// array of floats
+type tPoint [DIM]float64
+
+// tVertex is the define as a struct with the fields required for
+// the implementation of a polygon as a circular list, including
+// also some fields required for the triangulation algorithm
+type tVertex struct {
+	index       int64    // Vertex index
+	coord       tPoint   // Coordinates, coord[X] & coord[Y]
+	ear         bool     // ear is required to implement the triangulation algorithm
+	left, right *tVertex // pointers to the adjacent vertex
+}
+
+// tPolygon is a pointer to the first vertex
+type tPolygon *tVertex
 
 func main() {
-	var p1 = tpoint{rand.Float64(), rand.Float64()}
-	fmt.Printf("Point:\n\tX: %f\n\tY: %f\n", p1[X], p1[Y])
+	var aPolygon tPolygon
+
+	aPolygon = new(tVertex)
+	aPolygon.index = 0
+	aPolygon.ear = true
+	aPolygon.left = aPolygon
+	aPolygon.right = aPolygon
+	aPolygon.coord[X] = rand.Float64()
+	aPolygon.coord[Y] = rand.Float64()
+
+	fmt.Printf("Vertex:\n\tX: %f\n\tY: %f\n", aPolygon.coord[X], aPolygon.coord[Y])
 }
